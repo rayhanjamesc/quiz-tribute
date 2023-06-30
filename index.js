fb4142d46cf57eafc19912e741ebb4cd6238b131
rayhanjamesc <123619702+rayhanjamesc@users.noreply.github.com>
2023-06-30 15:47:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,142 @@
+let qOne = document.getElementById("question-one");
+let aOne = document.getElementById("answer-one");
+
+function getanswerOne() {
+    if (qOne.value.toLowerCase() == "andrea") {
+        aOne.textContent = "Correct";
+    } else {
+        aOne.textContent = "Incorrect";
+    }
+}
+
+let questionTwo = document.getElementById("question-two");
+var answerTwo = document.getElementById("answer-two");
+
+function getanswerTwo() {
+    if (questionTwo.value.toLowerCase() == "grunt") {
+        answerTwo.textContent = "Correct";
+    } else {
+        answerTwo.textContent = "Incorrect";
+    }
+}
+
+let questionThree = document.getElementById("question-three");
+let answerThree = document.getElementById("answer-three");
+
+function getanswerThree() {
+    if (questionThree.value.toLowerCase() == "rare") {
+        answerThree.textContent = "Correct";
+    } else {
+        answerThree.textContent = "Incorrect";
+    }
+}
+
+let questionFour = document.getElementById("question-four");
+let answerFour = document.getElementById("answer-four");
+
+function getanswerFour() {
+    if (questionFour.value.toLowerCase() == "eric") {
+        answerFour.textContent = "Correct";
+    } else {
+        answerFour.textContent = "Incorrect";
+    }
+}
+
+var questionFive = document.getElementById("question-five");
+var answerFive = document.getElementById("answer-five");
+
+function getanswerFive() {
+    if (questionFive.value.toLowerCase() == "august") {
+        answerFive.textContent = "Correct";
+    } else {
+        answerFive.textContent = "Incorrect";
+    }
+}
+
+var questionSix = document.getElementById("question-six");
+var answerSix = document.getElementById("answer-six");
+
+function getanswerSix() {
+    if (questionSix.value.toLowerCase() == "tickets") {
+        answerSix.textContent = "Correct";
+    } else {
+        answerSix.textContent = "Incorrect";
+    }
+}
+
+var questionSeven = document.getElementById("question-seven");
+var answerSeven = document.getElementById("answer-seven");
+
+function getanswerSeven() {
+    if (questionSeven.value.toLowerCase() == "floppy") {
+        answerSeven.textContent = "Correct";
+    } else {
+        answerSeven.textContent = "Incorrect";
+    }
+}
+
+var questionEight = document.getElementById("question-eight");
+var answerEight = document.getElementById("answer-eight");
+
+function getanswerEight() {
+    if (questionEight.value.toLowerCase() == "ren") {
+        answerEight.textContent = "Correct";
+    } else {
+        answerEight.textContent = "Incorrect";
+    }
+}
+
+var questionNine = document.getElementById("question-nine");
+var answerNine = document.getElementById("answer-nine");
+
+function getanswerNine() {
+    if (questionNine.value.toLowerCase() == "india") {
+        answerNine.textContent = "Correct";
+    } else {
+        answerNine.textContent = "Incorrect";
+    }
+}
+
+var questionTen = document.getElementById("question-ten");
+var answerTen = document.getElementById("answer-ten");
+
+function getanswerTen() {
+    if (questionTen.value.toLowerCase() == "epic") {
+        answerTen.textContent = "Correct";
+    } else {
+        answerTen.textContent = "Incorrect";
+    }
+}
+
+var questionEleven = document.getElementById("question-eleven");
+var answerEleven = document.getElementById("answer-eleven");
+
+function getanswerEleven() {
+    if (questionEleven.value.toLowerCase() == "new alice springs") {
+        answerEleven.textContent = "Correct";
+    } else {
+        answerEleven.textContent = "Incorrect";
+    }
+}
+
+var questionTwelve = document.getElementById("question-twelve");
+var answerTwelve = document.getElementById("answer-twelve");
+
+function getanswerTwelve() {
+    if (questionTwelve.value.toLowerCase() == "dog day afternoon") {
+        answerTwelve.textContent = "Correct";
+    } else {
+        answerTwelve.textContent = "Incorrect";
+    }
+}
+
+var questionFinal = document.getElementById("question-final");
+var answerFinal = document.getElementById("answer-final");
+
+function getanswerFinal() {
+    if (questionFinal.value.toLowerCase() == "a great friend") {
+        answerFinal.textContent = "Correct! Thank you so much Lee for being a great teacher, a really fun coworker, and a great friend. The whole Dustland fam wishes you good luck in your future endeavors!";
+    } else {
+        answerFinal.textContent = "Incorrect";
+    }
+}
